0033bf4915e2ef4b5620f168b20a4d07d8c36570
Stephen Haberman <stephen@exigencecorp.com>
2013-04-18 22:15:35 -0500

    Add a test for doneTodos firing a change.

--- src/test/java/org/tessell/todomvc/client/model/AppStateTest.java
+++ src/test/java/org/tessell/todomvc/client/model/AppStateTest.java
@@ -1,5 +1,31 @@
 package org.tessell.todomvc.client.model;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.tessell.model.events.PropertyChangedEvent;
+import org.tessell.model.events.PropertyChangedHandler;
+
 public class AppStateTest {
 
+  private final AppState state = new AppState();
+
+  @Test
+  public void doneTodosNoticesDoneChanged() {
+    Todo a = new Todo("a");
+    state.allTodos.add(a);
+
+    final boolean[] changed = { false };
+    state.doneTodos.addPropertyChangedHandler(new PropertyChangedHandler<List<Todo>>() {
+      public void onPropertyChanged(PropertyChangedEvent<List<Todo>> event) {
+        changed[0] = true;
+      }
+    });
+
+    a.done.set(true);
+    assertThat(changed[0], is(true));
+  }
 }
