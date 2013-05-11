f0b3cac11158dba3a4dbb8eb7a2be7e9e0ab715a
Stephen Haberman <stephen@exigencecorp.com>
2013-05-11 00:42:58 -0500

    Use a helper method for addAndReplaceElement.

--- src/main/java/org/tessell/todomvc/client/app/AppPresenter.java
+++ src/main/java/org/tessell/todomvc/client/app/AppPresenter.java
@@ -3,7 +3,9 @@ package org.tessell.todomvc.client.app;
 import static org.tessell.todomvc.client.views.AppViews.newAppView;
 import static org.tessell.todomvc.client.views.AppViews.newInfoView;
 
+import org.tessell.gwt.dom.client.IsElement;
 import org.tessell.presenter.BasicPresenter;
+import org.tessell.presenter.Presenter;
 import org.tessell.todomvc.client.model.AppState;
 import org.tessell.todomvc.client.views.IsAppView;
 
@@ -20,24 +22,17 @@ public class AppPresenter extends BasicPresenter<IsAppView> {
   public void onBind() {
     super.onBind();
 
-    view.root().addAndReplaceElement(//
-      addPresenter(new HeaderPresenter(state.allTodos)).getView(),
-      view.headerPlaceholder());
-
-    view.root().addAndReplaceElement(//
-      addPresenter(new MainPresenter(state)).getView(),
-      view.mainPlaceholder());
-
-    view.root().addAndReplaceElement(//
-      addPresenter(new FooterPresenter(state)).getView(),
-      view.footerPlaceholder());
-
-    view.root().addAndReplaceElement(//
-      newInfoView(),
-      view.infoPlaceholder());
+    add(new HeaderPresenter(state.allTodos), view.headerPlaceholder());
+    add(new MainPresenter(state), view.mainPlaceholder());
+    add(new FooterPresenter(state), view.footerPlaceholder());
+    view.root().addAndReplaceElement(newInfoView(), view.infoPlaceholder());
 
     binder.when(state.allTodos.size()).is(0).hide(view.main());
     binder.when(state.allTodos.size()).is(0).hide(view.footer());
+  }
+
+  private void add(Presenter p, IsElement placeholder) {
+    view.root().addAndReplaceElement(addPresenter(p).getView(), placeholder);
   }
 
 }
