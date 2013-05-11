b7ca80ea85b6147f0a96e65face86cf4077c91bc
Stephen Haberman <stephen@exigencecorp.com>
2013-05-10 22:01:19 -0500

    Hook up toggleAll.

--- src/main/java/org/tessell/todomvc/client/app/MainPresenter.java
+++ src/main/java/org/tessell/todomvc/client/app/MainPresenter.java
@@ -9,6 +9,9 @@ import org.tessell.todomvc.client.model.AppState;
 import org.tessell.todomvc.client.model.Todo;
 import org.tessell.todomvc.client.views.IsMainView;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+
 public class MainPresenter extends BasicPresenter<IsMainView> {
 
   private final AppState state;
@@ -21,9 +24,21 @@ public class MainPresenter extends BasicPresenter<IsMainView> {
   @Override
   public void onBind() {
     super.onBind();
+
     binder.bind(state.allTodos).to(this, view.ul(), new ListPresenterFactory<Todo>() {
       public Presenter create(Todo todo) {
         return new TodoPresenter(state, todo);
+      }
+    });
+
+    binder.bind(state.doneTodos.is(state.allTodos)).to(view.toggleAll());
+
+    view.toggleAll().addClickHandler(new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        boolean newValue = !view.toggleAll().getValue();
+        for (Todo todo : state.allTodos.get()) {
+          todo.done.set(newValue);
+        }
       }
     });
   }
--- src/test/java/org/tessell/todomvc/client/app/MainPresenterTest.java
+++ src/test/java/org/tessell/todomvc/client/app/MainPresenterTest.java
@@ -13,18 +13,18 @@ public class MainPresenterTest extends AbstractPresenterTest {
   final AppState state = new AppState();
   final MainPresenter p = bind(new MainPresenter(state));
   final StubMainView v = (StubMainView) p.getView();
-  
+
   @Test
   public void initiallyNoLines() {
     assertThat(v.ul().getWidgetCount(), is(0));
   }
-  
+
   @Test
   public void addsNewLineForNewTodo() {
     state.allTodos.add(new Todo("one"));
     assertThat(v.ul().getWidgetCount(), is(1));
   }
-  
+
   @Test
   public void removesLineForDeletedTodo() {
     Todo one = new Todo("one");
@@ -32,5 +32,41 @@ public class MainPresenterTest extends AbstractPresenterTest {
     assertThat(v.ul().getWidgetCount(), is(1));
     state.allTodos.remove(one);
     assertThat(v.ul().getWidgetCount(), is(0));
+  }
+
+  @Test
+  public void toggleAllIsInitiallyTrueBecauseDoneAndAllMatch() {
+    assertThat(v.toggleAll().isChecked(), is(true));
+  }
+
+  @Test
+  public void toggleAllIsFalseWhenOneUnchecked() {
+    state.allTodos.add(new Todo("one"));
+    assertThat(v.toggleAll().isChecked(), is(false));
+  }
+
+  @Test
+  public void toggleAllIsTrueWhenAllChecked() {
+    Todo one = new Todo("one");
+    state.allTodos.add(one);
+    one.done.set(true);
+    assertThat(v.toggleAll().isChecked(), is(true));
+  }
+
+  @Test
+  public void toggleAllSetsDoneForAllTodos() {
+    Todo one = new Todo("one");
+    state.allTodos.add(one);
+    v.toggleAll().check();
+    assertThat(one.done.get(), is(true));
+  }
+
+  @Test
+  public void toggleAllUnsetsDoneForAllTodos() {
+    Todo one = new Todo("one");
+    state.allTodos.add(one);
+    one.done.set(true);
+    v.toggleAll().uncheck();
+    assertThat(one.done.get(), is(false));
   }
 }
