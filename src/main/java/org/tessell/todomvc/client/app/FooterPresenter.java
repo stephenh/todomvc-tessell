8fc2a5b2d4dc56a0e71aa7fe3f823d7e29d04a17
Stephen Haberman <stephen@exigencecorp.com>
2013-05-12 23:31:58 -0500

    Inline leftText/clearText methods.

--- src/main/java/org/tessell/todomvc/client/app/FooterPresenter.java
+++ src/main/java/org/tessell/todomvc/client/app/FooterPresenter.java
@@ -1,9 +1,9 @@
 package org.tessell.todomvc.client.app;
 
 import static org.tessell.model.dsl.TakesValues.textOf;
+import static org.tessell.model.properties.NewProperty.stringProperty;
 import static org.tessell.todomvc.client.views.AppViews.newFooterView;
 
-import org.tessell.model.properties.StringProperty;
 import org.tessell.model.values.DerivedValue;
 import org.tessell.presenter.BasicPresenter;
 import org.tessell.todomvc.client.model.AppState;
@@ -26,33 +26,23 @@ public class FooterPresenter extends BasicPresenter<IsFooterView> {
     super.onBind();
 
     binder.bind(state.numberLeft.asString()).to(textOf(view.numberLeft()));
-    binder.bind(leftText(state)).to(textOf(view.numberLeftWord()));
-    binder.bind(clearText(state)).to(textOf(view.clearCompleted()));
+    binder.bind(stringProperty(new DerivedValue<String>() {
+      public String get() {
+        return state.numberLeft.get() == 1 ? "item" : "items";
+      }
+    })).to(textOf(view.numberLeftWord()));
+
     binder.when(state.doneTodos.size()).is(0).hide(view.clearCompleted());
+    binder.bind(stringProperty(new DerivedValue<String>() {
+      public String get() {
+        String itemText = state.doneTodos.get().size() == 1 ? "item" : "items";
+        return "Clear " + state.doneTodos.get().size() + " completed " + itemText;
+      }
+    })).to(textOf(view.clearCompleted()));
 
     view.clearCompleted().addClickHandler(new ClickHandler() {
       public void onClick(ClickEvent event) {
         state.removeDone();
-      }
-    });
-  }
-
-  /** @return "item" or "items" based on number left */
-  private static StringProperty leftText(final AppState state) {
-    return new StringProperty(new DerivedValue<String>() {
-      public String get() {
-        return state.numberLeft.get() == 1 ? "item" : "items";
-      }
-    });
-  }
-
-  /** @return "Clear X completed item(s) */
-  private static StringProperty clearText(final AppState state) {
-    return new StringProperty(new DerivedValue<String>() {
-      public String get() {
-        int done = state.doneTodos.get().size();
-        String itemText = done == 1 ? "item" : "items";
-        return "Clear " + done + " completed " + itemText;
       }
     });
   }
