f1d4e309c0b707efda036a69b9f94ca22653aec1
Stephen Haberman <stephen@exigencecorp.com>
2013-05-12 23:13:33 -0500

    Use binder.onKeyDown, as it will do the filter for us.

--- src/main/java/org/tessell/todomvc/client/app/HeaderPresenter.java
+++ src/main/java/org/tessell/todomvc/client/app/HeaderPresenter.java
@@ -1,15 +1,13 @@
 package org.tessell.todomvc.client.app;
 
+import static com.google.gwt.event.dom.client.KeyCodes.KEY_ENTER;
 import static org.tessell.todomvc.client.views.AppViews.newHeaderView;
+import static org.tessell.util.StringUtils.sanitizeIfString;
 
 import org.tessell.model.properties.ListProperty;
 import org.tessell.presenter.BasicPresenter;
 import org.tessell.todomvc.client.model.Todo;
 import org.tessell.todomvc.client.views.IsHeaderView;
-
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyDownEvent;
-import com.google.gwt.event.dom.client.KeyDownHandler;
 
 /** Creates new tasks when the user adds them. */
 public class HeaderPresenter extends BasicPresenter<IsHeaderView> {
@@ -24,13 +22,10 @@ public class HeaderPresenter extends BasicPresenter<IsHeaderView> {
   @Override
   public void onBind() {
     super.onBind();
-    view.newTodo().addKeyDownHandler(new KeyDownHandler() {
-      public void onKeyDown(KeyDownEvent event) {
-        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-          String newName = view.newTodo().getText().trim();
-          if ("".equals(newName)) {
-            return;
-          }
+    binder.onKeyDown(view.newTodo(), KEY_ENTER).execute(new Runnable() {
+      public void run() {
+        String newName = sanitizeIfString(view.newTodo().getText());
+        if (newName != null) {
           allTodos.add(new Todo(newName));
           view.newTodo().setText(null);
         }
